e2ea2a97704e920a9a83d0600ed328cb334a8997
manu <manuelugoccioni@gmail.com>
2023-10-29 02:52:30 +0200

    Cool percentages effect

--- src/routes/cookies/CookieStat.svelte
+++ src/routes/cookies/CookieStat.svelte
@@ -4,8 +4,14 @@
 </script>
 
 <section class="flex flex-col items-center">
-    <h1 style="font-size: 10rem;" class="font-bold">
+    <h1 style="font-size: 10rem;" class="font-bold percentage">
         {percentage}%
     </h1>
     <p class="text-gray-500 text-xl">{message}</p>
 </section>
+
+<style>
+    .percentage {
+        text-shadow: 0 10px 0 theme('colors.indigo.700');
+    }
+</style>
